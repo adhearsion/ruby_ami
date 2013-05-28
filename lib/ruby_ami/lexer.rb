2155e013397dbff04005de18c9c06927d69cdec5
Ben Klang <bklang@mojolingo.com>
2013-05-28 00:46:25 -0400

    Remove a bunch of dead code

--- lib/ruby_ami/lexer.rb
+++ lib/ruby_ami/lexer.rb
@@ -2,33 +2,6 @@
 
 module RubyAMI
   class Lexer
-
-    BufferOverrunError = Class.new IOError
-
-    KILOBYTE = 1024
-    BUFFER_SIZE = 128 * KILOBYTE unless defined? BUFFER_SIZE
-
-    ##
-    # IMPORTANT! See method documentation for adjust_pointers!
-    #
-    # @see  adjust_pointers
-    #
-    POINTERS = [
-      :@current_pointer,
-      :@token_start,
-      :@token_end,
-      :@version_start,
-      :@event_name_start,
-      :@current_key_position,
-      :@current_value_position,
-      :@last_seen_value_end,
-      :@error_reason_start,
-      :@follows_text_start,
-      :@current_syntax_error_start,
-      :@immediate_response_start
-    ]
-
-
     TOKENS = {
       cr: /\r/, #UNUSED?
       lf: /\n/, #UNUSED?
@@ -107,50 +80,11 @@ module RubyAMI
     end
 
     def extend_buffer_with(new_data)
-      length = new_data.size
-
-      if length > BUFFER_SIZE
-        raise Exception, "ERROR: Buffer overrun! Input size (#{new_data.size}) larger than buffer (#{BUFFER_SIZE})"
-      end
-
-      if length + @data.size > BUFFER_SIZE
-        if @data.size != @current_pointer
-          if @current_pointer < length
-            # We are about to shift more bytes off the array than we have
-            # parsed.  This will cause the parser to lose state so
-            # integrity cannot be guaranteed.
-            raise BufferOverrunError, "ERROR: Buffer overrun! AMI parser cannot guarantee sanity. New data size: #{new_data.size}; Current pointer at #{@current_pointer}; Data size: #{@data.size}"
-          end
-        end
-        @data.slice! 0...length
-        adjust_pointers -length
-      end
       @data << new_data
       @data_ending_pointer = @data.size
     end
 
     protected
-
-    ##
-    # This method will adjust all pointers into the buffer according
-    # to the supplied offset.  This is necessary any time the buffer
-    # changes, for example when the sliding window is incremented forward
-    # after new data is received.
-    #
-    # It is VERY IMPORTANT that when any additional pointers are defined
-    # that they are added to this method.  Unpredictable results may
-    # otherwise occur!
-    #
-    # @see https://adhearsion.lighthouseapp.com/projects/5871-adhearsion/tickets/72-ami-lexer-buffer-offset#ticket-72-26
-    #
-    # @param offset Adjust pointers by offset.  May be negative.
-    #
-    def adjust_pointers(offset)
-      POINTERS.each do |ptr|
-        value = instance_variable_get(ptr)
-        instance_variable_set(ptr, value + offset) if !value.nil?
-      end
-    end
 
     ##
     # Called after a response or event has been successfully parsed.
@@ -159,18 +93,6 @@ module RubyAMI
     #
     def message_received(message)
       @delegate.message_received message
-    end
-
-    ##
-    # Called when there is an Error: stanza on the socket. Could be caused by executing an unrecognized command, trying
-    # to originate into an invalid priority, etc. Note: many errors' responses are actually tightly coupled to a
-    # Event which comes directly after it. Often the message will say something like "Channel status
-    # will follow".
-    #
-    # @param [String] reason The reason given in the Message: header for the error stanza.
-    #
-    def error_received(message)
-      @delegate.error_received message
     end
 
     ##
@@ -187,76 +109,6 @@ module RubyAMI
         obj[key] = value
       end
       obj
-    end
-
-    def error_reason_starts
-      @error_reason_start = @current_pointer
-    end
-
-    def error_reason_stops
-      @current_message.message = @data[@error_reason_start...@current_pointer]
-    end
-
-    def follows_text_starts
-      @follows_text_start = @current_pointer
-    end
-
-    def follows_text_stops
-      text = @data[@last_seen_value_end..@current_pointer]
-      text.sub! /\r?\n--END COMMAND--/, ""
-      @current_message.text_body = text
-      @follows_text_start = nil
-    end
-
-    def add_pair_to_current_message
-      @current_message[@current_key] = @current_value
-      reset_key_and_value_positions
-    end
-
-    def reset_key_and_value_positions
-      @current_key, @current_value, @current_key_position, @current_value_position = nil
-    end
-
-    def syntax_error_starts
-      @current_syntax_error_start = @current_pointer # Adding 1 since the pointer is still set to the last successful match
-    end
-
-    def syntax_error_stops
-      # Subtracting 3 from @current_pointer below for "\r\n" which separates a stanza
-      offending_data = @data[@current_syntax_error_start...@current_pointer - 1]
-      syntax_error_encountered offending_data
-      @current_syntax_error_start = nil
-    end
-
-    def immediate_response_starts
-      @immediate_response_start = @current_pointer
-    end
-
-    def immediate_response_stops
-      message = @data[@immediate_response_start...(@current_pointer -1)]
-      message_received Response.from_immediate_response(message)
-    end
-
-    ##
-    # This method is used primarily in debugging.
-    #
-    def view_buffer(message = nil)
-      message ||= "Viewing the buffer"
-
-      buffer = @data.clone
-      buffer.insert(@current_pointer, "\033[0;31m\033[1;31m^\033[0m")
-
-      buffer.gsub!("\r", "\\\\r")
-      buffer.gsub!("\n", "\\n\n")
-
-      puts <<-INSPECTION
-VVVVVVVVVVVVVVVVVVVVVVVVVVVVV
-####  #{message}
-#############################
-#{buffer}
-#############################
-^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-      INSPECTION
     end
   end
 end
