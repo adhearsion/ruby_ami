b628229681cf4d500c1ee944dcda1e2630514335
Ben Langfeld <ben@langfeld.me>
2013-07-03 17:29:40 -0300

    [CS] Do a bunch less unescaping of AGI environment strings
    Boosts performance (on MRI 2.0.0) by approx. 1.6x

--- lib/ruby_ami/async_agi_environment_parser.rb
+++ lib/ruby_ami/async_agi_environment_parser.rb
@@ -3,13 +3,16 @@ require 'cgi'
 
 module RubyAMI
   class AsyncAGIEnvironmentParser
+    NEWLINE = "%0A".freeze
+    COLON_SPACE = '%3A%20'.freeze
+
     def initialize(environment_string)
       @environment_string = environment_string.dup
     end
 
     def to_hash
       to_array.inject({}) do |accumulator, element|
-        accumulator[element[0].to_sym] = element[1] || ''
+        accumulator[element[0].to_sym] = CGI.unescape(element[1] || '')
         accumulator
       end
     end
@@ -21,7 +24,7 @@ module RubyAMI
     private
 
     def to_array
-      CGI.unescape(@environment_string).split("\n").map { |p| p.split ': ' }
+      @environment_string.split(NEWLINE).map { |p| p.split COLON_SPACE }
     end
   end
 end
